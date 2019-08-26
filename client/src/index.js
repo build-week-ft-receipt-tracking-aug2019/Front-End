62785ea5b3a4f925d0c1eba106044a361118299b
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-26 18:12:18 -0400

    fixed dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
@@ -16,4 +15,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
