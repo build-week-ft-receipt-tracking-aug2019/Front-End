7da17b775ef420b7a2f43485e0d48e79c5fc8ead
antilou86 <antilou86@gmail.com>
2019-08-26 17:15:39 -0400

    NAV-WIP: fleshed out initial nav-bar shape and added menu items and colorization using semantic UI. moving on to adding Routing and Links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ client/src/components/Nav.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Menu, Button } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+class Nav extends Component {
+  state = { activeItem: 'dashboard' }
+
+  handleItemClick = (e, { name }) => {
+      e.preventDefault();
+      this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu inverted size='large' color='green' fluid widths={4}>
+        <Menu.Item fitted
+          name='dashboard'
+          active={activeItem === 'dashboard'}
+          onClick={this.handleItemClick} >
+              <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMSHN0F9V/reciepttrackerlogo.png" alt="logo" className="logoBoi"/>
+        </Menu.Item>
+
+        <Menu.Item
+          name='Add Receipt'
+          active={activeItem === 'Add Receipt'}
+          onClick={this.handleItemClick}
+        > Add Receipt +
+        </Menu.Item>
+
+        <Menu.Item
+          name='Sign Up'
+          active={activeItem === 'Sign Up'}
+          onClick={this.handleItemClick}
+        > Sign Up
+        </Menu.Item>
+
+        <Menu.Item
+          name='Log In'
+          active={activeItem === 'Log In'}
+          onClick={this.handleItemClick}
+        >
+            <Button primary>Log In</Button>
+        </Menu.Item>
+
+      </Menu>
+    )
+  }
+}
+
+
+export default Nav
