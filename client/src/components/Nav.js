86fcf2e9455e44d9ea47acfb71740dd3767bb819
antilou86 <antilou86@gmail.com>
2019-08-27 11:53:53 -0400

    NAV complete

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -7,52 +7,50 @@ class Nav extends Component {
   state = { activeItem: 'dashboard' }
 
   handleItemClick = (e, { name }) => {
-      e.preventDefault();
-      this.setState({ activeItem: name })
+    e.preventDefault();
+    this.setState({ activeItem: name })
   }
 
   render() {
     const { activeItem } = this.state
 
     return (
-      <Menu inverted size='large' color='green' fluid widths={4}>
+      <>
+
+        <Menu inverted size='large' color='green' fluid widths={3}>
           {/* {the icon looks a little -wonky- will update during polish} */}
+
+
+          <Menu.Item
+            name='Add Receipt'
+            active={activeItem === 'Add Receipt'}
+            onClick={this.handleItemClick}
+          ><NavLink to="/add-receipt"> Add Receipt +</NavLink>
+          </Menu.Item>
+
+
+
+          <Menu.Item
+            name='Sign Up'
+            active={activeItem === 'Sign Up'}
+            onClick={this.handleItemClick}
+          ><NavLink to="/sign-up"> Sign Up</NavLink>
+          </Menu.Item>
+
+
+          <Menu.Item
+            name='Log In'
+            active={activeItem === 'Log In'}
+            onClick={this.handleItemClick}
+          >
+            <NavLink to="/login"><Button primary>Log In</Button></NavLink>
+          </Menu.Item>
+
+        </Menu>
         <NavLink to='/'>
-        <Menu.Item fitted
-          name='dashboard'
-          active={activeItem === 'dashboard'}
-          onClick={this.handleItemClick} >
-              <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMSHN0F9V/reciepttrackerlogo.png" alt="logo" className="logoBoi"/>
-        </Menu.Item>
+          <img className="ReceiptLogo" src="https://files.slack.com/files-pri/T4JUEB3ME-FMQ7Z2Z60/image.png" alt="logo" className="logoBoi" />
         </NavLink>
-
-        <NavLink to="/add-receipt">
-        <Menu.Item
-          name='Add Receipt'
-          active={activeItem === 'Add Receipt'}
-          onClick={this.handleItemClick}
-        > Add Receipt +
-        </Menu.Item>
-        </NavLink>
-
-        <NavLink to="/sign-up">
-        <Menu.Item
-          name='Sign Up'
-          active={activeItem === 'Sign Up'}
-          onClick={this.handleItemClick}
-        > Sign Up
-        </Menu.Item>
-        </NavLink>
-        
-        <Menu.Item
-          name='Log In'
-          active={activeItem === 'Log In'}
-          onClick={this.handleItemClick}
-        >
-           <NavLink to="/login"><Button primary>Log In</Button></NavLink>
-        </Menu.Item>
-        
-      </Menu>
+      </>
     )
   }
 }
