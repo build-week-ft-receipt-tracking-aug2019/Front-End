5d590e7c014ca8dc656324162d362b7a912d8872
antilou86 <antilou86@gmail.com>
2019-08-28 13:50:31 -0400

    Delete function is now being called correcty (thank you chris), receiving 404 error. testing with newly created receipt.

--- client/src/actions/deleteReceipt.js
+++ client/src/actions/deleteReceipt.js
@@ -1,14 +1,11 @@
 import { axiosWithAuth } from '../utilities/axiosWithAuth';
-import axios from 'axios';
 import {DELETE_RECEIPT_SUCCESS, ERROR} from './index';
 import {getReceipts} from './getReceipts';
 
 export const deleteReceipt = id => dispatch => {
         console.log('does this log?');
-        axios
-            .delete(`https://receipt-tracker-api.herokuapp.com/users/receipt/${id}`, {
-                headers: {authorization: localStorage.getItem('token')}
-            })
+        axiosWithAuth()
+            .delete(`https://receipt-tracker-api.herokuapp.com/users/receipt/${id}`)
             .then(res => {
                 console.log("delete response===",res);
                 dispatch({ type: DELETE_RECEIPT_SUCCESS });
